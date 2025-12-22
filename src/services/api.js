9ce36bfd0120e26c01f1d6be6d5dbd6e3e5ad871
TechShri027 <shrishtibansal2705@gmail.com>
2025-12-22 15:11:20 +0530

    url added

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://backend-blog-1-tdc2.onrender.com",
 });
 
 // token auto attach
